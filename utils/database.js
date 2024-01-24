58e720e00d3e952d7520143c44a9c950eff80790
Ariful Islam <ariful6@yahoo.com>
2024-01-24 04:38:39 +0200

    commit

--- utils/database.js
+++ utils/database.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 
-let isConnected = false;
+let isConnected = false; // track the connection
 
 export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
 
   if (isConnected) {
     console.log("MongoDB is already connected");
+    return;
   }
 
   try {
