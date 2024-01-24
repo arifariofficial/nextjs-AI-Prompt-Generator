c31aa5537ec397ca831f90648af83ce35431eba2
Ariful Islam <ariful6@yahoo.com>
2024-01-24 02:31:23 +0200

    fixex

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -2,11 +2,14 @@
 import Profile from "@components/Profile";
 import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const MyProfile = () => {
   const { data: session } = useSession();
 
   const [myPosts, setMyPosts] = useState([]);
+
+  const router = useRouter();
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -21,12 +24,25 @@ const MyProfile = () => {
     }
   }, [session?.user.id]);
 
-  const handleEdit = async (arg) => {
-    return;
+  const handleEdit = async (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
   };
 
-  const handleDelete = async (arg) => {
-    return;
+  const handleDelete = async (post) => {
+    const hasConfired = confirm("Do you want to delete this prompt");
+
+    if (hasConfired) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPost = myPosts.filter((item) => item._id !== post._id);
+        setMyPosts(filteredPost);
+      } catch (error) {
+        console.log(error);
+      }
+    }
   };
 
   return (
